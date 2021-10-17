a3eadbf38e4d005560999c14c2df35363a183305
Kristof <you@example.com>
2021-10-17 21:41:51 +0200

    Elemek inicializálása

---
+++ app/src/main/java/com/example/thinkanumber/MainActivity.java
@@ -0,0 +1,40 @@
+package com.example.thinkanumber;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ImageView imageView1, imageView2;
+    private Button btn1, btn2, btn3, btn4;
+    private TextView textView1;
+    private String eredmenyek;
+    private boolean ketKocka;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        init();
+
+
+    }
+
+    private void init(){
+        imageView1 = findViewById(R.id.imgView1);
+        imageView2 = findViewById(R.id.imgView2);
+        btn1 = findViewById(R.id.btn1);
+        btn2 = findViewById(R.id.btn2);
+        btn3 = findViewById(R.id.btn3);
+        btn4 = findViewById(R.id.btn4);
+        textView1 = findViewById(R.id.textView1);
+        eredmenyek = "";
+        ketKocka = true;
+    }
+}
